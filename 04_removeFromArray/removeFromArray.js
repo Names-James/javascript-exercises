a6a413fdc8d7c8619f10d18e021b8d46ff339857
names-james-home <contact.jameslloyd@gmail.com>
2021-10-30 23:16:27 -0400

    6P/1F on removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
 const removeFromArray = function() {
+    const values = arguments[0];
+    const arg = [];
 
+    for (i = 1; i < arguments.length; i++) {
+            arg.push(arguments[i]);
+    };
+
+
+    for (i = 0; i <= arg.length; i++) {
+        for (r=0; r <= values.length; r++) {
+            if (values[r] == arg[i]) {
+                values.splice(r, 1);
+            }
+        }
+    };
+
+return values;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
