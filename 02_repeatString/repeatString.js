b6b78eb103b42b627a89ff0c7337e4a16d758e94
names-james-home <contact.jameslloyd@gmail.com>
2021-10-30 21:36:42 -0400

    First two solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, reps) {
+    let newStr = '';
+    if (reps < 0) {
+        newStr = 'ERROR';
+    } else {
+        for (i = 0; i < reps; i++) {
+        newStr += str;
+        };
+    };
+    return newStr;
 };
 
 // Do not edit below this line
