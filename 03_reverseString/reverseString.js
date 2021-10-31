3ef0849db10d40449e67253e0c70dd69d55552bf
names-james-home <contact.jameslloyd@gmail.com>
2021-10-30 21:58:55 -0400

    Third exercise solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
 const reverseString = function(str) {
-
+let newString = '';
+for (i=0; i <= str.length; i++) {
+    newString += str.charAt(str.length - i);
+};
+return newString;
 };
 
 // Do not edit below this line
