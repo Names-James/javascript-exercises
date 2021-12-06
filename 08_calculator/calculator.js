8ca2854e8879f5bf2c76b7e0aa03dd6703fd8d45
names-james-home <contact.jameslloyd@gmail.com>
2021-12-06 12:59:13 -0500

    calculator solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,23 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (array) => array.reduce((previousValue, currentValue) => {
+  return previousValue + currentValue;
+}, 0);
 
-const multiply = function() {
+const multiply = (array) => array.reduce((previousValue, currentValue) => {
+  return previousValue * currentValue;
+});
 
-};
+const power = (a, b) => a ** b;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (num) => {
+  let newTotal = 1;
+  for (i = 1; i <= num; i++) {
+    newTotal *= i;
+  }
+  return newTotal;
 };
 
 // Do not edit below this line
